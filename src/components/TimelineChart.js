17be7584dc6a2ffba133627d65174449aaf1de25
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-04-29 00:54:05 +1000

    adding separate colors for each series

--- src/components/TimelineChart.js
+++ src/components/TimelineChart.js
@@ -9,31 +9,35 @@ class TimelineChart extends React.Component {
 			series: [{
 				name: 'Bob',
 				data: [{
-					x: 'React',
+					x: 'React', fillColor: '#008FFB',
 					y: [new Date('2023-02-05').getTime(), new Date('2023-04-03').getTime()]
 				},
 				{
-					x: 'Angular',
-					y: [new Date('2019-11-18').getTime(), new Date('2023-01-20').getTime()]
+					x: 'Angular', fillColor: '#FF4560',
+					y: [new Date('2019-11-18').getTime(), new Date('2023-04-30').getTime()]
 				},
 				{
-					x: 'NodeJS',
-					y: [new Date('2018-05-03').getTime(), new Date('2018-10-30').getTime()]
+					x: 'NodeJS', fillColor: '#6cc24a',
+					y: [new Date('2018-05-03').getTime(), new Date('2019-05-30').getTime()]
 				},
 				{
-					x: 'VueJS',
+					x: 'React', fillColor: '#008FFB',
+					y: [new Date('2018-11-20').getTime(), new Date('2019-11-14').getTime()]
+				},
+				{
+					x: 'VueJS', fillColor: '#42b883',
 					y: [new Date('2018-05-01').getTime(), new Date('2018-10-30').getTime()]
 				},
 				{
-					x: 'Angular',
+					x: 'Angular', fillColor: '#FF4560',
 					y: [new Date('2017-10-01').getTime(), new Date('2018-04-30').getTime()]
 				},
 				{
-					x: 'AngularJS',
+					x: 'AngularJS', fillColor: '#FF4560',
 					y: [new Date('2015-06-01').getTime(), new Date('2017-11-30').getTime()]
 				},
 				{
-					x: 'Typescript',
+					x: 'Typescript', fillColor: '#007acc',
 					y: [new Date('2015-06-20').getTime(), new Date('2023-05-01').getTime()]
 				}]
 			}],

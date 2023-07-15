141001613b9ef2dbddc7c6fc8dddd55da21432a7
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-07-16 01:24:13 +1000

    skills page, components cleanup

---
+++ src/pages/skills/SkillsPage.tsx
@@ -0,0 +1,15 @@
+import './SkillsPage.scss';
+
+import TimelineChart from './components/TimelineChart/TimelineChart';
+
+const SkillsPage = () => {
+	return (
+		<div id="skills-page">
+			<div className="content">
+				<TimelineChart />
+			</div>
+		</div>
+	)
+}
+
+export default SkillsPage;

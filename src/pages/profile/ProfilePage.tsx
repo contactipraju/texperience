5449f09d89a616c005d0d61368cc9d69a50f6583
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-08-03 19:03:01 +1000

    Anchors must have content, that must be accessible by a screen reader. fixed

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -27,7 +27,7 @@ const ProfilePage = () => {
 
 			<div className="container">
 				<div className='lhs'>
-					<div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="prasadaraju" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://au.linkedin.com/in/prasadaraju?trk=profile-badge"></a></div>
+					<div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="prasadaraju" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://au.linkedin.com/in/prasadaraju?trk=profile-badge"> </a></div>
 				</div>
 				<div className='rhs'>
 					<ProfileForm />

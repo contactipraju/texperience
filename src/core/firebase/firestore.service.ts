c3a5622c9de21ed994dcbdb93ed6bd407c2afb28
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-08-10 16:04:59 +1000

    storing logs separately for localhost, my-ip and general visits

--- src/core/firebase/firestore.service.ts
+++ src/core/firebase/firestore.service.ts
@@ -11,6 +11,10 @@ function isLocalHost() {
 	return (["localhost", "127.0.0.1", ""].includes(window.location.hostname));
 }
 
+function isMyIp(ipAddress: string) {
+	return (["125.209.150.67"].includes(ipAddress));
+}
+
 function getUserDataUrl() {
 	// Since Firebase only allows connections to https,
 	// we use a different api though it provides less information
@@ -22,25 +26,27 @@ async function getUserData() {
 	return {...user.data, _time: new Date()};
 }
 
-function logEventToDB(_type: string, data: any) {
+function logEventToDB(_type: string, user: any) {
 	let type = _type;
 	type += isLocalHost() ? "_local" : "";
+	type += isMyIp(user['ipAddress']) ? "_mine" : "";
 
 	const ref = collection(firestore, type);
 
 	try {
-		addDoc(ref, data)
+		addDoc(ref, user);
 	} catch(err) {
-		console.log(err)
+		console.log(err);
 	}
 }
 
 export async function LogEvent(url: any, type: string) {
 	const user = await getUserData();
+
 	user['_url'] = url;
 
 	// Log to Firebase Analytics, except download events
-	if (type.localeCompare('resume_downloads')) {
+	if (type.localeCompare('resume-downloads')) {
 		logEvent(analytics, type, user);
 	}
 
